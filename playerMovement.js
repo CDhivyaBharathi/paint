b4e9c60f4096b7ee0f08e371817bbfdf8ac2da7d
C Dhivya Bharathi <bharathi.chellakumar@gmail.com>
2021-11-16 19:41:43 +0530

    paint ball

---
+++ playerMovement.js
@@ -0,0 +1,19 @@
+AFRAME.registerComponent("playermovement",{
+    init:function(){
+        this.walk()
+    },
+    walk:function(){
+        window.addEventListener("keydown", (e) => {
+            if (
+                e.key === "ArrowUp" ||
+                e.key === "ArrowRight" ||
+                e.key === "ArrowLeft" ||
+                e.key === "ArrowDown"
+            ){
+                var entity = document.querySelector("#sound2");
+                entity.component.sound.playSound()
+            }
+            
+        })
+    }
+})
